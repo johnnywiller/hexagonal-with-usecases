03d82160cbdfecc02dc2027086bdac5a7c056183
j.goncalves <j.goncalves@ocado.com>
2022-10-29 19:48:43 +0200

    some javadoc

--- src/main/java/hexagonal/app/payment/domain/port/driver/CreatePaymentUseCaseFactory.java
+++ src/main/java/hexagonal/app/payment/domain/port/driver/CreatePaymentUseCaseFactory.java
@@ -2,6 +2,9 @@ package hexagonal.app.payment.domain.port.driver;
 
 import hexagonal.app.payment.domain.CreatePaymentCommand;
 
+/**
+ * Factory for usecases that create a payment
+ */
 public interface CreatePaymentUseCaseFactory {
 
     CreatePaymentUseCase useCase();
