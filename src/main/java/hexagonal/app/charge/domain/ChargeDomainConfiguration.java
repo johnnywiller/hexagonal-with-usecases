9e8668d335c5de90c16ad9fd89d7a5a08c719945
Johnny Goncalves <j.goncalves@ocado.com>
2022-12-17 17:00:59 +0100

    make the factory a bean

--- src/main/java/hexagonal/app/charge/domain/ChargeDomainConfiguration.java
+++ src/main/java/hexagonal/app/charge/domain/ChargeDomainConfiguration.java
@@ -4,8 +4,11 @@ import hexagonal.app.charge.domain.port.driven.GetReservationPort;
 import hexagonal.app.charge.domain.port.driven.MakeChargePort;
 import hexagonal.app.charge.domain.port.driver.ChargeUseCaseFactory;
 import hexagonal.app.shared.EventPublisherPort;
+import org.springframework.context.annotation.Bean;
 
 public class ChargeDomainConfiguration {
+
+    @Bean
     ChargeUseCaseFactory createChargeUseCaseFactory(EventPublisherPort publisher,
                                                     MakeChargePort makeChargePort,
                                                     GetReservationPort reservationPort) {
