fc2f9c8b72562931e58788fd15d23494c4ddbd1b
Johnny Goncalves <j.goncalves@ocado.com>
2023-05-07 20:00:29 +0200

    delombok
    Signed-off-by: Johnny Goncalves <j.goncalves@ocado.com>

--- src/main/java/hexagonal/app/payment/domain/DefaultCreatePaymentUseCase.java
+++ src/main/java/hexagonal/app/payment/domain/DefaultCreatePaymentUseCase.java
@@ -1,19 +1,28 @@
 package hexagonal.app.payment.domain;
 
+import org.joda.money.Money;
+
 import hexagonal.app.payment.domain.port.driven.CardReservationPort;
 import hexagonal.app.payment.domain.port.driven.PaymentIdentityPort;
 import hexagonal.app.payment.domain.port.driver.UseCase;
 import hexagonal.app.shared.EventPublisherPort;
-import lombok.Value;
-import org.joda.money.Money;
 
-@Value
 public class DefaultCreatePaymentUseCase implements UseCase {
 
-    EventPublisherPort eventPublisher;
-    PaymentIdentityPort paymentIdentityPort;
-    CardReservationPort cardReservationPort;
-    CreatePaymentCommand command;
+    private final EventPublisherPort eventPublisher;
+    private final PaymentIdentityPort paymentIdentityPort;
+    private final CardReservationPort cardReservationPort;
+    private final CreatePaymentCommand command;
+
+    public DefaultCreatePaymentUseCase(EventPublisherPort eventPublisher,
+                                       PaymentIdentityPort paymentIdentityPort,
+                                       CardReservationPort cardReservationPort,
+                                       CreatePaymentCommand command) {
+        this.eventPublisher = eventPublisher;
+        this.paymentIdentityPort = paymentIdentityPort;
+        this.cardReservationPort = cardReservationPort;
+        this.command = command;
+    }
 
     @Override
     public void execute() {
