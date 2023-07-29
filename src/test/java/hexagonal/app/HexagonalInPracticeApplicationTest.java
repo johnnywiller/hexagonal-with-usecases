7bd10fd640597482d4dd9ab83f8c472d0da4f5ec
Johnny Goncalves <j.goncalves@ocado.com>
2023-07-29 10:49:02 +0200

    add logback
    Signed-off-by: Johnny Goncalves <j.goncalves@ocado.com>

--- src/test/java/hexagonal/app/HexagonalInPracticeApplicationTest.java
+++ src/test/java/hexagonal/app/HexagonalInPracticeApplicationTest.java
@@ -2,17 +2,16 @@ package hexagonal.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.modulith.core.ApplicationModules;
-import org.springframework.modulith.docs.Documenter;
 
 class HexagonalInPracticeApplicationTest {
 
     @Test
     void writeDocumentationSnippets() {
-        var modules = ApplicationModules.of(HexagonalInPracticeApplication.class).verify();
+        ApplicationModules modules = ApplicationModules.of(HexagonalInPracticeApplication.class).verify();
+        modules.verify();
+        modules.forEach(System.out::println);
 
-        new Documenter(modules)
-                .writeModulesAsPlantUml()
-                .writeIndividualModulesAsPlantUml();
+
     }
 
 }
