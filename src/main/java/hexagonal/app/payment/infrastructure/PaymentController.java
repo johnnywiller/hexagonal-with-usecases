9078c009b1125229a3ca761f83d44b456497e9fd
Johnny Goncalves <j.goncalves@ocado.com>
2025-12-27 18:36:32 +0100

    delombok
    Signed-off-by: Johnny Goncalves <j.goncalves@ocado.com>

--- src/main/java/hexagonal/app/payment/infrastructure/PaymentController.java
+++ src/main/java/hexagonal/app/payment/infrastructure/PaymentController.java
@@ -2,6 +2,7 @@ package hexagonal.app.payment.infrastructure;
 
 import hexagonal.app.payment.domain.Payment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PaymentController {
