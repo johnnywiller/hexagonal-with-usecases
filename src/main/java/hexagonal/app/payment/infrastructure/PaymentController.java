c43a03c3fc39f38474bbacf499e2d66750c5892f
Johnny Goncalves <j.goncalves@ocado.com>
2025-12-27 18:34:40 +0100

    java 25
    Signed-off-by: Johnny Goncalves <j.goncalves@ocado.com>

--- src/main/java/hexagonal/app/payment/infrastructure/PaymentController.java
+++ src/main/java/hexagonal/app/payment/infrastructure/PaymentController.java
@@ -2,11 +2,9 @@ package hexagonal.app.payment.infrastructure;
 
 import hexagonal.app.payment.domain.Payment;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PaymentController {
-
     @GetMapping
     public Payment getPayment() {
         return new Payment();
