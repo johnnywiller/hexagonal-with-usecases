7bc3efb9a955a81a7bfe4e420c4edf870cdc6d1e
Johnny Willer <johnnywiller10@gmail.com>
2025-12-27 18:43:13 +0100

    Add frontend page (#36)
    * add svelte files
    
    Signed-off-by: Johnny Goncalves <j.goncalves@ocado.com>
    
    * add redirect
    
    Signed-off-by: Johnny Goncalves <j.goncalves@ocado.com>
    
    ---------
    
    Signed-off-by: Johnny Goncalves <j.goncalves@ocado.com>
    Co-authored-by: Johnny Goncalves <j.goncalves@ocado.com>

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,88 @@
+<script>
+  import '../app.css';
+</script>
+
+<main>
+  <div class="logo-container">
+    <img src="spring-boot.svg" alt="SpringBoot Logo" class="logo spring-boot" />
+    <span class="plus">+</span>
+    <img src="svelte.svg" alt="Svelte Logo" class="logo svelte" />
+  </div>
+
+  <h1>Spring Boot + Svelte</h1>
+
+  <p>
+    This is a statically-served SvelteKit landing page inside a Spring Boot
+    application.
+  </p>
+  <p class="read-the-docs">
+    The Svelte app is built as a set of static files which are then served by
+    Spring Boot.
+  </p>
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    --primary-color: #ff3e00;
+    --secondary-color: #6db33f;
+    --text-color: #333;
+    --background-color: #f4f4f4;
+  }
+
+  main {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    height: 100vh;
+    background-color: var(--background-color);
+    color: var(--text-color);
+  }
+
+  .logo-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 2rem;
+  }
+
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em var(--primary-color));
+  }
+  .logo.spring-boot:hover {
+    filter: drop-shadow(0 0 2em var(--secondary-color));
+  }
+  .plus {
+    font-size: 3em;
+    color: #ccc;
+    margin: 0 1rem;
+  }
+
+  h1 {
+    color: var(--primary-color);
+    text-transform: uppercase;
+    font-size: 4rem;
+    font-weight: 100;
+    line-height: 1.1;
+    margin: 0 auto 1rem;
+  }
+
+  p {
+    max-width: 520px;
+    margin: 1rem auto;
+    line-height: 1.5;
+  }
+
+  .read-the-docs {
+    color: #888;
+  }
+</style>
--- src/main/java/hexagonal/app/payment/infrastructure/PaymentController.java
+++ src/main/java/hexagonal/app/payment/infrastructure/PaymentController.java
@@ -2,9 +2,11 @@ package hexagonal.app.payment.infrastructure;
 
 import hexagonal.app.payment.domain.Payment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/payment")
 public class PaymentController {
     @GetMapping
     public Payment getPayment() {
